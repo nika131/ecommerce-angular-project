3172c06c8324930010aa28731da31c014391db90
NIickk230 <n.megr23@gmail.com>
2026-01-13 05:06:18 +0400

    full project

--- src/app/core/pages/homepage1/homepage1.ts
+++ src/app/core/pages/homepage1/homepage1.ts
@@ -2,10 +2,11 @@ import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductService } from '../../services/product.service';
 import { HighlightOnHover } from '../../../shared/directives/highlight-on-hover';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-homepage1',
-  imports: [CommonModule, HighlightOnHover],
+  imports: [CommonModule, HighlightOnHover, RouterLink],
   templateUrl: './homepage1.html',
   styleUrl: './homepage1.css',
 })
